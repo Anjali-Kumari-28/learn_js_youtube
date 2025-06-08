bbffa1396ec82cd9c36ad764f684e7a4308cf3b1
Anjali <anjali280205@gamil.com>
2025-06-08 18:47:44 +0530

    Add 01_variables.js

---
+++ Js_initial.js/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 65245
+const name = "Anjali"
+let accountEmail = "anjali@gmail.com"
+var accountPassword = "54321"
+let accountCity = "Patna"
+let accountState = "Bihar"
+let accountCountry;
+
+// accountId = 2  // not allowed
+
+// accountEmail = "abc@vst.com"
+// accountPassword = "764635"
+// accountCity = "Bengaluru"
+// accountState = "Karnataka"
+
+// console.log(accountEmail);
+
+console.table([accountId, name, accountEmail, accountPassword, accountCity, accountState, accountcountry])
