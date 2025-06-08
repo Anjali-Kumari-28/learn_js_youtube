15fea84437eb976655f18d0fe3527f89e4016f1a
Anjali <anjali280205@gamil.com>
2025-06-09 01:19:51 +0530

    modify

--- Js_initial.js/03_conversionOperation.js
+++ Js_initial.js/03_conversionOperation.js
@@ -1,12 +1,13 @@
 let score = undefined
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
+//comments
 //"33" => 33
 //"33abc" => NaN
 //true => 1; false => 0
@@ -17,6 +18,8 @@ let isLoggedIn = "Anjali"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 // console.log(booleanIsLoggedIn);
+
+//comments
 //1 => true(1), 0 => false(0)
 //"" => false
 // "Anjali" => true
@@ -24,5 +27,42 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 32
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ************* Operations *************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/4);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2= " Anjali"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log((3 + 4) * 5 % 3);.
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2 
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
