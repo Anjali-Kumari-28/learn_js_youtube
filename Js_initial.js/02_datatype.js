ab08d4f35723b49b4f4e15b1802270ace0109559
Anjali <anjali280205@gamil.com>
2025-06-09 00:00:50 +0530

    datatypes

---
+++ Js_initial.js/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as nevwer version
+
+// alert(3 + 3) //we are using node.js,not browser
+
+console.log(3 + 5) //code readability should be high
+
+console.log("Anjali")
+
+let name = "Anjali"
+let age = 20
+let isLoggedIn = true
+
+// number => 2 to power 53
+//bigint
+//string => "abc"
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => uniqueness
+
+//object
+// console.log(typeof "Anjali");
+// console.log(typeof age);
+// console.log(typeof null); // type - object
+
+console.log(typeof undefined); // type - undefined
