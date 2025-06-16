ba3f1da1a9837b8885124e36c80ead4fdd620898
Anjali <anjali280205@gamil.com>
2025-06-16 19:46:40 +0530

    Datatypes summary

--- Js_initial.js/02_datatype.js
+++ Js_initial.js/02_datatype.js
@@ -2,13 +2,13 @@
 
 // alert(3 + 3) //we are using node.js,not browser
 
-console.log(3 + 5) //code readability should be high
+// console.log(3 + 5) //code readability should be high
 
-console.log("Anjali")
+// console.log("Anjali")
 
 let name = "Anjali"
 let age = 20
-let isLoggedIn = true
+// let isLoggedIn = true
 
 // number => 2 to power 53
 //bigint
@@ -23,4 +23,36 @@ let isLoggedIn = true
 // console.log(typeof age);
 // console.log(typeof null); // type - object
 
-console.log(typeof undefined); // type - undefined
+// console.log(typeof undefined); // type - undefined
+
+
+
+//Data types
+// 1 primitive data type
+
+// 7types: string, number, boolean, null, undefined, symbol, BigInt
+
+const score = 100
+const scorevalue = 100.3
+
+const isLoggedIn = false
+const outsideTem = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// Non primitive (Reference)
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "ramcharan"];
+let myObj = {
+    name: "anjali",
+    age: 20,
+}
+const myFunction = function(){
+    console.log("Hello world");
+}
+console.log(typeof anotherId);
